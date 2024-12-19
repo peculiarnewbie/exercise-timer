3d86c61699fbd56dd05ec55328f498c200615ec7
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-12-19 20:59:54 +0700

    multiple timers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,75 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 import TimeTick from "@/components/TimeTick";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function TabOneScreen() {
-	const [initTime, setInitTime] = useState(60);
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Tab One</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<TimeTick initTime={initTime} />
-			<EditScreenInfo path="app/(tabs)/index.tsx" />
-		</View>
-	);
+    const [initTimes, setInitTimes] = useState([10]);
+    const [isRunning, setIsRunning] = useState(true);
+    const [timerIndex, setTimerIndex] = useState(0);
+
+    const togglePause = () => {
+        setIsRunning((prev) => !prev);
+    };
+
+    const nextTimer = () => {
+        setTimerIndex((prev) => (prev + 1) % initTimes.length);
+    };
+
+    const addTime = () => {
+        setInitTimes((prev) => [...prev, 10]);
+    };
+
+    // Use a ref to track running state
+
+    return (
+        <View style={styles.container}>
+            <View style={{ flex: 2 }} />
+            <View style={styles.flex}>
+                {initTimes.map((initTime, index) => (
+                    <TimeTick
+                        initTime={initTime}
+                        isRunning={isRunning}
+                        isActive={timerIndex === index}
+                        nextTimer={nextTimer}
+                    />
+                ))}
+            </View>
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
+            <View style={styles.flex}>
+                <Button onPress={togglePause} title="Pause" />
+                <Button onPress={nextTimer} title="Skip" />
+                <Button onPress={addTime} title="Add Time" />
+            </View>
+            <View style={{ flex: 2 }} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
+    flex: {
+        flex: 0.4,
+        flexDirection: "row",
+        gap: 10,
+    },
 });
--- components/TimeTick.tsx
+++ components/TimeTick.tsx
@@ -2,38 +2,52 @@ import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { Text, View } from "./Themed";
 import { Button, GestureResponderEvent } from "react-native";
 
-export default function TimeTick(props: { initTime: number }) {
-	const [elapsedTime, setElapsedTime] = useState(0);
-	const [isRunning, setIsRunning] = useState(true);
+export default function TimeTick(props: {
+    initTime: number;
+    isRunning: boolean;
+    isActive: boolean;
+    nextTimer: () => void;
+}) {
+    const [elapsedTime, setElapsedTime] = useState(0);
 
-	const intervalRef = useRef(null) as MutableRefObject<NodeJS.Timeout | null>;
+    const intervalRef = useRef(null) as MutableRefObject<NodeJS.Timeout | null>;
 
-	const togglePause = () => {
-		console.log(isRunning);
-		setIsRunning((prev) => !prev);
-		isRunningRef.current = !isRunningRef.current;
-	};
+    const isRunningRef = useRef(props.isRunning);
 
-	// Use a ref to track running state
-	const isRunningRef = useRef(isRunning);
+    useEffect(() => {
+        isRunningRef.current = props.isRunning;
+    }, [props.isRunning]);
 
-	useEffect(() => {
-		console.log("effect");
-		intervalRef.current = setInterval(() => {
-			if (!isRunningRef.current) return;
-			setElapsedTime((prevCount) => prevCount + 0.1);
-		}, 100);
-		return () => {
-			if (intervalRef.current) {
-				clearInterval(intervalRef.current);
-			}
-		};
-	}, []);
+    useEffect(() => {
+        if (props.isActive) {
+            intervalRef.current = setInterval(() => {
+                if (!isRunningRef.current) return;
+                setElapsedTime((prevCount) => prevCount + 0.1);
+            }, 100);
+        } else {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+            setElapsedTime(0);
+        }
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
+    }, [props.isActive]);
 
-	return (
-		<View>
-			<Text>{(props.initTime - elapsedTime).toFixed(0)}</Text>
-			<Button onPress={togglePause} title="Pause" />
-		</View>
-	);
+    useEffect(() => {
+        if (elapsedTime > props.initTime) {
+            props.nextTimer();
+        }
+    }, [elapsedTime]);
+
+    return (
+        <View>
+            <Text style={{ fontSize: 40 }}>
+                {(props.initTime - elapsedTime).toFixed(0)}
+            </Text>
+        </View>
+    );
 }
